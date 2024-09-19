be6564c1bc023621c9ab3d8ea8d9229ac663735e
Manh <manhnus@gmail.com>
2024-09-19 13:19:10 +0700

    CSRF token

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,7 +17,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf)
 	// And then create the routes using the appropriate methods, patterns and
 	// handlers.
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
